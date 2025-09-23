57c67fc6e1f6105c40bf373e225ac53bfdd8efe7
NadiaDNA <nadiadininurazizah@gmail.com>
2025-09-23 22:34:57 +0700

    change emoji into icon with lucide-react

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -17,8 +17,9 @@
 // }
 
 import { useCartStore } from './store/cartStore';
-import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import { Handbag } from 'lucide-react';
+import { Store } from 'lucide-react';
 
 
 export default function Navbar() {
@@ -26,15 +27,20 @@ export default function Navbar() {
 
   return (
     <nav className="bg-[#701A42] p-4 text-white flex justify-between items-center">
-      <h1 className="text-xl font-bold">🛍️ DNAStore</h1>
-      <Button variant="ghost" className="relative hover:opacity-80 transition-opacity duration-200">
-        <span className="text-2xl">👜</span>
+      {/* <h1 className="text-xl font-bold">🛍️ DNAStore</h1> */}
+        <div className="flex items-center gap-2 ml-2">
+            <Store size={30} />
+            <h1 className='text-2xl'><span className="text-2xl font-bold">DNA_</span>Store</h1>
+        </div>
+      <div variant="ghost" className="h-auto w-auto mr-3 relative hover:opacity-80 transition-opacity duration-200">
+        {/* <span className="text-2xl">👜</span> */}
+        <Handbag size={30} />
         {totalItems > 0 && (
-          <Badge className="absolute -top-0.5 -right-0.5 rounded-full bg-white text-[#701A42]" variant="destructive">
+          <Badge className="absolute -top-2 -right-4 rounded-full bg-white text-[#701A42]" variant="destructive">
             {totalItems}
           </Badge>
         )}
-      </Button>
+      </div>
     </nav>
   );
 }
